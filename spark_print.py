502b1f6f454516e530fe25536e6018ff5eeb6400
thiru56139 <55854781+thiru56139@users.noreply.github.com>
2021-05-25 19:34:26 -0400

    Add files via upload

---
+++ spark_print.py
@@ -0,0 +1,29 @@
+from pyspark.context import SparkContext
+from pyspark.sql.session import SparkSession
+sc = SparkContext('local')
+spark = SparkSession(sc)
+
+df = spark \
+.read \
+.format("kafka") \
+.option("kafka.bootstrap.servers", "172.25.0.12:9092,172.25.0.13:9092") \
+.option("startingOffsets", "earliest") \
+.option("subscribe", "AAPL") \
+.load()
+
+df1 = df.selectExpr("CAST(key AS STRING)", "CAST(CAST(value AS STRING) AS FLOAT)", "timestamp")
+
+df1.show()
+
+#SQL table
+
+df1.createOrReplaceTempView("apple_stockprice")
+
+highest = spark.sql("SELECT key as time, value as highest_price_day FROM apple_stockprice WHERE value IN (select max(value) FROM apple_stockprice)")
+highest.show()
+
+lowest = spark.sql("SELECT key as time, value as lowest_price_day FROM apple_stockprice WHERE value IN (select min(value) FROM apple_stockprice)")
+lowest.show()
+
+standard = spark.sql("SELECT stddev(value) as standard_deviation_day FROM apple_stockprice")
+standard.show()
