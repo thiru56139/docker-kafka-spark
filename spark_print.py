3858f67da16ee741a649ab6bc68fc31338190dd4
thiru56139 <55854781+thiru56139@users.noreply.github.com>
2021-05-27 10:50:31 -0400

    Update spark_print.py

--- spark_print.py
+++ spark_print.py
@@ -8,7 +8,7 @@ df = spark \
 .format("kafka") \
 .option("kafka.bootstrap.servers", "172.25.0.12:9092,172.25.0.13:9092") \
 .option("startingOffsets", "earliest") \
-.option("subscribe", "AAPL") \
+.option("subscribe", "a5_acc") \
 .load()
 
 df1 = df.selectExpr("CAST(key AS STRING)", "CAST(CAST(value AS STRING) AS FLOAT)", "timestamp")
